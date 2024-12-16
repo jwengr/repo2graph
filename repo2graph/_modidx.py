4ddb5df8eafc5cafbc2a8762cf6d56cc8ee784d5
jwengr <jinwoo0766@naver.com>
2024-12-16 23:17:43 +0900

    fix

--- repo2graph/_modidx.py
+++ repo2graph/_modidx.py
@@ -7,6 +7,5 @@ d = { 'settings': { 'branch': 'main',
                 'lib_path': 'repo2graph'},
   'syms': { 'repo2graph.core': { 'repo2graph.core.add_node_by_path': ('core.html#add_node_by_path', 'repo2graph/core.py'),
                                  'repo2graph.core.add_node_by_repo': ('core.html#add_node_by_repo', 'repo2graph/core.py'),
-                                 'repo2graph.core.extract_class_function_relationships_with_source': ( 'core.html#extract_class_function_relationships_with_source',
-                                                                                                       'repo2graph/core.py')},
+                                 'repo2graph.core.parse_py': ('core.html#parse_py', 'repo2graph/core.py')},
             'repo2graph.utils': {'repo2graph.utils.clone_repo': ('utils.html#clone_repo', 'repo2graph/utils.py')}}}
